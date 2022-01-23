b7bbce8a957e6970a516d90490455cb632abd718
Manoj Kumar Bandaru <manoj123067.bm@gmail.com>
2022-01-24 01:21:44 +0530

    Adding and using ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import ExpenseItem from "./components/js/ExpenseItem";
+
 const App = () => {
+  const expenses = [
+    {
+      date: new Date(2022, 4, 7),
+      title: 'breakfast',
+      amount: 1000
+    },
+    {
+      date: new Date(2022, 4, 7),
+      title: 'lunch',
+      amount: 2000
+    },
+    {
+      date: new Date(2022, 4, 7),
+      title: 'breakfast',
+      amount: 1500
+    }
+  ];
+
   return (
     <div>
-      <h2>Let's get started Arjun!</h2>
-      <p>This is also visible</p>
+      <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount} expense={expenses[0]} />
+      <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount} expense={expenses[0]} />
+      <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount} expense={expenses[0]} />
     </div>
   );
 }
