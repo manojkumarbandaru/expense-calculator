10db3fd56f2c6d7dbfef033fa8d2c1c1e1f329b4
Manoj Kumar Bandaru <manoj123067.bm@gmail.com>
2022-02-09 00:14:54 +0530

    dynamically rendering the expenses using state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import ExpenseItem from "./components/js/ExpenseItem";
 import Card from "./components/js/Card";
 import NewExpense from "./components/js/NewExpense";
 
 const App = () => {
-  const expenses = [
+  const INTIAL_EXPENSES = [
     {
       id: '1',
       date: new Date(2022, 4, 7),
@@ -24,16 +25,23 @@ const App = () => {
     }
   ];
 
+  const [expenses, setExpenses] = useState(INTIAL_EXPENSES);
+
   const addNewExpenseHandler = (expenseData) => {
     console.log(expenseData);
+    setExpenses((prevState) => {
+      return [expenseData, ...prevState]
+    });
   };
 
   return (
     <Card>
       <NewExpense addNewExpense={addNewExpenseHandler} />
-      <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount} />
-      <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount} />
-      <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount} />
+      {
+        expenses.map((expense) => {
+          return <ExpenseItem key={expense.id} date={expense.date} title={expense.title} amount={expense.amount} />
+        })
+      }
     </Card>
   );
 }
--- src/components/js/ExpenseItem.js
+++ src/components/js/ExpenseItem.js
@@ -1,24 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import '../css/ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-    const buttonClickHandler = () => {
-        setTitle('Updated!');
-        console.log(title);
-    };
-
     return (
         <Card className='expense-item'>
             {/* content b/n Card tag is passed as children prop to Card component */}
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
-                <button onClick={buttonClickHandler}>change Title</button>
             </div>
         </Card>
     );
